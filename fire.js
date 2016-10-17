639a27b68e4b31ff04deaf8698618ce00d35ed02
mappingkat <katrina@fulcrumapp.com>
2016-10-17 16:33:40 -0600

    adding different port

--- fire.js
+++ fire.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fulcrumMiddleware = require('connect-fulcrum-webhook');
 var request = require('request');
-var PORT = process.env.PORT || 9999;
+var PORT = process.env.PORT || 9001;
 var app = express();
 
 function payloadProcessor (payload, done) {
